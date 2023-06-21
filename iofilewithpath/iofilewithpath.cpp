bf1e89a9b02cef1395a4b2e57b436bcf188a024c
muhfarrasm <muhammadfarrasm@gmail.com>
2023-06-21 15:27:57 +0700

    Membuat   infile membaca

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -8,7 +8,7 @@ int main() {
 	string baris;
 	string NamaFile;
 
-	cout << "Masukkan Nama File";
+	cout << "Masukkan Nama File = ";
 	cin >> NamaFile;
 
 	//membuka file dengan mode menulis;
@@ -29,5 +29,30 @@ int main() {
 		outfile << baris << endl;
 	}
 
+	//Sekarang tutup filenya
+	outfile.close();
 
+	//Membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open(NamaFile+ ".txt", ios::out);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris 
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file jika sudah selesai 
+		infile.close();
+	}
+	//Jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }
+
+
